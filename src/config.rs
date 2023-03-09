8bd4109141559fc916ef935514e092d6f0153005
cauvmou <cauvmou.8855@gmail.com>
2023-03-09 15:59:24 +0100

    wip

--- src/config.rs
+++ src/config.rs
@@ -1,2 +1,8 @@
 pub const MAX_DATAGRAM_SIZE: usize = 1350;
 pub const STREAM_BUFFER_SIZE: usize = 64 * 1024;
+
+/// Stores the configuration for multiple connections.
+/// Is translated in either `quiche::Config` or 
+pub struct Config {
+
+}
